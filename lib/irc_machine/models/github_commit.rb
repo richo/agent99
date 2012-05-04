04748b2d08d2bcf4f10728605f6c20f4bdf3a230
Rich Healey <richard.healey@99designs.com>
2012-05-04 15:13:00 +1000

    Use the authors nicks explicitly

--- lib/irc_machine/models/github_commit.rb
+++ lib/irc_machine/models/github_commit.rb
@@ -20,7 +20,7 @@ module IrcMachine
       end
 
       def notification_format(build_status)
-        "Build of #{commit.repo_name.irc_bold}/#{commit.branch.irc_bold} was a #{build_status} #{commit.repository.url}/compare/#{commit.before[0..6]}...#{commit.after[0..6]} in #{build_time.irc_bold}s PING #{authors.join(" ")}"
+        "Build of #{commit.repo_name.irc_bold}/#{commit.branch.irc_bold} was a #{build_status} #{commit.repository.url}/compare/#{commit.before[0..6]}...#{commit.after[0..6]} in #{build_time.irc_bold}s PING #{authors.map(&:nick).join(" ")}"
       end
 
     end
